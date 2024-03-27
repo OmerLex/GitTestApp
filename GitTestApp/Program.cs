d9c9aefa0b88e6f7db5161a0508271edc6bdcab1
Ömer F. Sözcü <omer.sozcu@lexutb.se>
2024-03-27 11:50:40 +0100

    #1 test-branch is created

--- GitTestApp/Program.cs
+++ GitTestApp/Program.cs
@@ -3,3 +3,6 @@ Console.WriteLine("Hello, World!");
 
 // This line is the first code of the project
 Console.WriteLine("This is first line");
+
+// This line is the added from test-branch
+Console.WriteLine("This is added from test-branch");
